2e81e963c539055630c58f8ef4316ff817b8ab92
Caden Kline <caden.kline@ll.mit.edu>
2023-07-06 15:09:49 -0400

    clean up python file and logging

--- nvram_plugin.py
+++ nvram_plugin.py
@@ -7,44 +7,43 @@ import os
 Interface to LibNvram Hypercalls.
 
 Register callbacks via 
-panda.pyplugins.ppp.NVramHC.ppp_reg_cb("NVRAM_GET",get_ipdb_call)
+panda.pyplugins.ppp.NVramHC.ppp_reg_cb("nvram_get",get_ipdb_call)
 For NVRAM_GET exmaple
 TODO: Setup actual list add. Handle additional nvram hooks
 '''
+MAGIC_VAL = 0x31838188
+ERROR_VAL = 0x12345678
 class NVramHC(PyPlugin):
     def __init__(self, panda: Panda):
         print("Loading Nvram HyperCall plugin")
         super().__init__(panda)
         self.panda = panda
         outdir = self.get_arg("outdir")
-        self.write_file = os.path.join(outdir,"nvram.log")
-        open(self.write_file, "w").close() #Clear the file
-        self.write2_file = os.path.join(outdir,"nvram2.log")
-        open(self.write2_file, "w").close() #Clear the file
+        self.log = self.get_arg_bool("log")
+        if self.log:
+            self.write_file = os.path.join(outdir,"nvram.log")
+            open(self.write_file, "w").close() #Clear the file
         self.nvram:dict = self.get_arg("nvram")
         if self.nvram is None:
             self.nvram = {}
         self.create_callbacks()
-        self.MAGIC_VAL = 0x31838188
-        self.ERROR_VAL = 0x12345678
+
         self.conf = 0
         @panda.cb_guest_hypercall
         def hypercall(cpu):
-            if panda.arch.get_arg(cpu, 0, convention="syscall") != self.MAGIC_VAL:
+            if panda.arch.get_arg(cpu, 0, convention="syscall") != MAGIC_VAL:
                 print(f"Found hypercall, but value was {hex(panda.arch.get_arg(cpu, 0, convention='syscall'))}")
                 return False
             len = panda.arch.get_arg(cpu, 1, convention='syscall')
             pointer_size = panda.bits //8
             argptr =  panda.arch.get_arg(cpu, 2, convention='syscall')
             argstr = []
-
             for x in range(0,len):
                 addr = int.from_bytes(panda.virtual_memory_read(cpu, argptr + ( pointer_size * x), pointer_size),'little')
                 argstr.append(panda.read_str(cpu, addr))
-            #print(argstr)
-            with open(self.write2_file, "a") as f:
+            with open(self.write_file, "a") as f:
                 f.write(f"\n{argstr}\n")
-            block = self.ppp_run_cb("NVRAM_BLOCK_ON_ALL",cpu,argstr)
+            block = self.ppp_run_cb("nvram_block_all",cpu,argstr)
             if block:
                 return True
             output = argstr[0]
@@ -59,14 +58,14 @@ class NVramHC(PyPlugin):
             elif "NVRAM_SET" in output:
                 self.set(cpu,argstr)
             elif "NVRAM_LIST_ADD" in output:
-                if not self.ppp_run_cb("NVRAM_LIST_ADD"):
+                if not self.ppp_run_cb("nvram_list_add",cpu,argstr):
                     self.set(cpu,argstr)
             elif "NVRAM_CLEAR" in output:
                 self.nvram.clear()
             elif "NVRAM_GETALL" in output:
                 self.getall(cpu,argstr)
             elif "INIT" in output:
-                if not self.ppp_run_cb("NVRAM_INIT"):
+                if not self.ppp_run_cb("nvram_init",cpu):
                     self.panda.arch.set_retval(cpu,self.conf)
             else:
                 print(argstr)
@@ -104,19 +103,19 @@ class NVramHC(PyPlugin):
         try:
             result = self.nvram[key]
             vali = int(result.decode().strip('\x00'))
-            if not self.ppp_run_cb("NVRAM_GET_INT",cpu,key,vali):
+            if not self.ppp_run_cb("nvram_get_int",cpu,key,vali):
                 self.panda.arch.set_retval(cpu,vali,convention='syscall')
         except:
             self.panda.arch.set_retval(cpu, 0,convention='syscall')
          
     def set(self, cpu, args):
-        #import ipdb;ipdb.set_trace()
         key = args[1]
         buf = args[2]
-        if not self.ppp_run_cb("NVRAM_SET",cpu,key,buf):
+        if not self.ppp_run_cb("nvram_set",cpu,key,buf):
             self.add_nvram(key,buf.encode()+b'\0')
-        with open(self.write_file, "a") as f:
-            f.write(f"\n{self.nvram}\n")
+        if self.log:
+            with open(self.write_file, "a") as f:
+                f.write(f"\n{self.nvram}\n")
         
     def get_buf(self, cpu, args):
         key = args[1]
@@ -138,6 +137,9 @@ class NVramHC(PyPlugin):
         key = args[1]
         try:
             self.nvram.pop(key)
+            if self.log:
+                with open(self.write_file, "a") as f:
+                    f.write(f"\n{self.nvram}\n")
             self.panda.arch.set_retval(cpu, 1)
         except:
             self.panda.arch.set_retval(cpu, 0)
@@ -145,8 +147,11 @@ class NVramHC(PyPlugin):
     def set_int(self,cpu,args):
         key = args[1]
         vali = int(args[2])
-        if not self.ppp_run_cb("NVRAM_SET_INT",cpu,key,vali):
+        if not self.ppp_run_cb("nvram_set_int",cpu,key,vali):
             self.nvram[key] = args[2].encode() + b'\0'
+            if self.log:
+                with open(self.write_file, "a") as f:
+                    f.write(f"\n{self.nvram}\n")
             self.panda.arch.set_retval(cpu,int.from_bytes(vali.to_bytes(self.panda.bits, 'little',signed=True), 'little'),'syscall')
     
     def getall(self,cpu,args):
@@ -155,27 +160,27 @@ class NVramHC(PyPlugin):
         try:
             b = self.nvram_byte_str()
             size = b[:sz].rfind(b"\0")
-            if not self.ppp_run_cb("NVRAM_GETALL",cpu,buf,sz,b,b[:size+1]):
+            if not self.ppp_run_cb("nvram_getall",cpu,buf,sz,b,b[:size+1]):
                 self.panda.virtual_memory_write(cpu, buf,b[:size+1])
                 self.panda.arch.set_retval(cpu, 1,'syscall')
         except:
             self.panda.arch.set_retval(cpu, self.MAGIC_VAL,'syscall')
     '''Current CALLBACKS
-        NVRAM_BLOCK_ON_ALL
-        NVRAM_INIT
-        NVRAM_GET
-        NVRAM_GETALL
-        NVRAM_SET
-        NVRAM_GET_INT
-        NVRAM_SET_INT
-        NVRAM_LIST_ADD
+        nvram_block_all(cpu, argstr)
+        nvram_init(cpu)
+        nvram_get(cpu,key,buf_ptr,size, shortened_buf,entire_buf)
+        nvram_getall(cpu,key,vali)
+        nvram_set(cpu,key,buf)
+        nvram_get_int(cpu,key,vali)
+        nvram_set_int(cpu,key,vali)
+        nvram_list_add(cpu,argstr)
     '''
     def create_callbacks(self):
-        self.ppp_cb_boilerplate("NVRAM_BLOCK_ON_ALL")
-        self.ppp_cb_boilerplate("NVRAM_INIT")
-        self.ppp_cb_boilerplate("NVRAM_GET")
-        self.ppp_cb_boilerplate("NVRAM_GETALL")
-        self.ppp_cb_boilerplate("NVRAM_SET")
-        self.ppp_cb_boilerplate("NVRAM_GET_INT")
-        self.ppp_cb_boilerplate("NVRAM_SET_INT")
-        self.ppp_cb_boilerplate("NVRAM_LIST_ADD")
+        self.ppp_cb_boilerplate("nvram_block_all")
+        self.ppp_cb_boilerplate("nvram_init")
+        self.ppp_cb_boilerplate("nvram_get")
+        self.ppp_cb_boilerplate("nvram_getall")
+        self.ppp_cb_boilerplate("nvram_set")
+        self.ppp_cb_boilerplate("nvram_get_int")
+        self.ppp_cb_boilerplate("nvram_set_int")
+        self.ppp_cb_boilerplate("nvram_list_add")
